90d97ea1dbc047198e744464fcf7559b9aa5dd8b
Bill Kirtley <bill@actbluetech.com>
2025-02-17 16:39:25 -0500

    More fun with routing (cats)

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Request, Query, Param } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('cats')
 export class CatsController {
-    @Get()
-    findAll(): string {
-      return 'This action returns all cats';
-    }  
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }  
+
+  @Get('breed')
+  findBreed(): Object {
+  //   return 'This action returns cat breeds';
+    return { breed: 'Siamese' };
+  }
+
+  @Get('color')
+  findColor(@Query() color: string, @Res() response) {
+    response.status(202).send({ color: color, wtf: 'dude' });
+  }
+
+  @Get('name/:name')
+  findName(@Param() name?: string) {
+    return { name: name };
+  }
 }
