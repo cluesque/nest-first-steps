97b7f83340ee366cdad250480821b13c6b41df0b
Bill Kirtley <bill@actbluetech.com>
2025-02-17 19:22:07 -0500

    Add config module for .env parsing
    $ npm i --save @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { RoutesModule } from './commands/routes.module';
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    RoutesModule
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
