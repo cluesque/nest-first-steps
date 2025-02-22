3aa1849b6b30aecc872f410ab70802d492fbbda9
Bill Kirtley <bill@actbluetech.com>
2025-02-22 16:46:29 -0500

    Demonstrate mocking responses

--- src/fetcher/fetcher.service.spec.ts
+++ src/fetcher/fetcher.service.spec.ts
@@ -30,13 +30,34 @@ describe('FetcherService', () => {
     expect(service.getCat).toBeDefined();
   });
 
-  it('should fetch the cat', async () => {    
+  it('should empty 200 by default', async () => {    
     let url = 'https://http.cat/201';
     let response = await service.getCat('nope');
     let body = await response.text();
 
     expect(response.status).toBe(200);
+    expect(response.headers.get('content-type')).toBe('text/plain;charset=UTF-8');
     expect(body).toBe('');  
+  });
+
+  it('allows mocking fetch', async () => {
+    fetchMock.mockResponseOnce('mocked response');
+    let response = await service.getCat('nope');
+    let body = await response.text();
+
+    expect(response.status).toBe(200);
+    expect(response.headers.get('content-type')).toBe('text/plain;charset=UTF-8');
+    expect(body).toBe('mocked response');  
+  });
+
+  it('can mock status and headers', async () => {
+    fetchMock.mockResponseOnce('hi mom', { status: 204, headers: { 'x-something-fake': 'narf' } });
+    let response = await service.getCat('nope');
+    let body = await response.text();
+
+    expect(response.status).toBe(204);
+    expect(response.headers.get('x-something-fake')).toBe('narf');
+    expect(body).toBe('hi mom');  
   });
 
   it('allows disabling the mock', async () => {
